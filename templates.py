6a97a910f4e154c7ae0e6d1476f82f77314e1af7
Eren Aydın <49097712+sscorpi@users.noreply.github.com>
2023-04-26 11:39:00 +0300

    Changed .htaccess template

--- templates.py
+++ templates.py
@@ -1,10 +1,6 @@
 from re import sub, DOTALL
 
-HTACCESS = """php_value display_errors Off
-RewriteEngine On
-RewriteCond %{REQUEST_FILENAME} !-d
-RewriteCond %{REQUEST_FILENAME}\.php -f
-RewriteRule ^(.*)$ $1.php [NC,L]"""
+HTACCESS = """ErrorDocument 404 /error.html"""
 
 # Read App.php file
 with open('C:/xampp/htdocs/App.php', 'r') as f:
