400aa3f0cd558d316a8cf6965f33de50b99fe6fd
Eren Aydın <49097712+sscorpi@users.noreply.github.com>
2023-04-26 10:50:55 +0300

    Added App.php (without comments) file content as HTML_SKELETON

--- templates.py
+++ templates.py
@@ -1,5 +1,13 @@
+from re import sub, DOTALL
+
 HTACCESS = """php_value display_errors Off
 RewriteEngine On
 RewriteCond %{REQUEST_FILENAME} !-d
 RewriteCond %{REQUEST_FILENAME}\.php -f
 RewriteRule ^(.*)$ $1.php [NC,L]"""
+
+# Read App.php file
+with open('C:/xampp/htdocs/App.php', 'r') as f:
+    code = f.read()
+
+HTML_SKELETON = sub(r'<!--.*?-->', '', code, flags=DOTALL)
