b945fbcdca4d8cfd756b7975151de697ed265e98
Eren Aydın <49097712+sscorpi@users.noreply.github.com>
2023-04-30 22:19:47 +0300

    Imported source path value from config.json

--- templates.py
+++ templates.py
@@ -1,4 +1,5 @@
 from re import sub, DOTALL
+from utils import SOURCE_PATH
 
 HTACCESS = """RewriteEngine On
 RewriteCond %{REQUEST_FILENAME} !-d
@@ -6,7 +7,7 @@ RewriteCond %{REQUEST_FILENAME}\.html -f
 RewriteRule ^(.*)$ $1.html [NC,L] """
 
 # Read App.php file
-with open("C:/xampp/htdocs/App.php", "r") as f:
+with open(f"{SOURCE_PATH}\\App.php", "r") as f:
     code = f.read()
 
 HTML_SKELETON = sub(r"<!--.*?-->", "", code, flags=DOTALL)
